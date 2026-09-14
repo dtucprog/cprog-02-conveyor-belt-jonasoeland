f8ec3057d5b9d84901ab861fc27d9ef7476c5292
Jonas <Jonasoeland@gmail.com>
2026-09-14 16:56:35 +0200

    Update conveyor-belt-capacity-check.c

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -4,6 +4,19 @@ int MOTOR_CAPACITY = 12;
 
 int main() {
     int motorCount, totalPackageWeight;
+    printf ("How many motors are carrying the packages?\n");
+    scanf ("%d", &motorCount);
+    /// Hvor mange motorer der er til at bære pakkerne. Outputtet er printf og scanf tager svaret ind.
 
-    /// YOUR CODE HERE
+    printf ("How many kg of packages do we expect?\n");
+    scanf ("%d", &totalPackageWeight);
+    /// Hvor mange kg pakkerne vi forventer, vejer. Outputtet er printf og scanf tager svaret ind.
+
+    if (totalPackageWeight<=motorCount*MOTOR_CAPACITY) {
+        printf ("Yes! The conveyor belt can carry the packages.\n");
+
+        } else { 
+            printf("No. The conveyor belt cannot carry the packages.\n");
+        }
+    /// Linjen der tager inputtet, sætter det op mod hinanden og laver forskellige outputs alt efter inputtet.
 }
